0582f60d4624ac4f88648207a9962fc3170d6547
ianwong123 <83454614+ianwong123@users.noreply.github.com>
2025-04-22 18:44:18 +0800

    Delete generate_qr.py

--- generate_qr.py
+++
@@ -1,5 +0,0 @@
-import qrcode
-# Generate Grounded CC QR
-qrcode.make("https://ianwong123.github.io/cc-qr/grounded-cc.jpg").save("grounded-qr.png")
-# Generate Coated CC QR
-qrcode.make("https://ianwong123.github.io/cc-qr/coated-cc.jpg").save("coated-qr.png")
